d7395a293c33daa35d42c75cc49183ebf62baff2
Elisha Tetteyfio <93135004+Elisha-Tetteyfio@users.noreply.github.com>
2025-10-03 22:47:23 +0000

    user signup

---
+++ app/controllers/auth/registrations_controller.rb
@@ -0,0 +1,18 @@
+class Auth::RegistrationsController < ApplicationController
+  # POST /auth/signup
+  def create
+    @user = User.new(signup_params)
+
+    if @user.save
+      render json: @user.as_json(except: [:password_digest]), status: :created, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_content
+    end
+  end
+
+  private
+
+    def signup_params
+      params.expect(registration: [ :first_name, :last_name, :email, :password, :phone_number, :user_type ])
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_secure_password
+  enum :user_type, superadmin: "SUPERADMIN", merchant: "MERCHANT"
 end
